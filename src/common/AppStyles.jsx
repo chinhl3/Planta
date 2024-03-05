b51c285ba70ecf827377af1c0cf6166b9b6f1b81
buihuunghia2004 <nghiabuihuu2004@gmail.com>
2024-03-05 09:27:59 +0700

    nghia

---
+++ src/common/AppStyles.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    containerInput:{
+        width:300,
+        height:46,
+        borderRadius:10,
+        flexDirection:'row',
+        paddingHorizontal:10,
+        alignItems:'center'
+    },
+    icon:{
+        width:24,height:24
+    }
+});
+export default styles;
--- src/planta/screens/auth/Log_in.jsx
+++ src/planta/screens/auth/Log_in.jsx
@@ -1,11 +1,63 @@
 import { AppState, Image, StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Img_header from '../../../common/Img_header'
+import AppInput from '../../../common/AppInput'
+import styles from '../../../common/AppStyles'
+
 const Log_in = () => {
+
+    const getContainerInputFocusStyle = () => {
+        return {
+            ...styles.containerInput,
+            borderColor: '#009245',
+            borderWidth: 2
+        }
+    };
+    const getContainerInputUnFocusStyle = () => {
+        return {
+            ...styles.containerInput,
+            borderColor: '#8B8B8B',
+            borderWidth: 1,
+        }
+    };
+
+    const getIconEyeStyle = () => {
+        return {
+            width: 29,
+            height: 24
+        }
+    };
+
+    const getInputTextStyle=()=>{
+        return{
+            flex:1
+        }
+    }
+
     return (
         <View>
-            <Img_header 
+            <Img_header
                 require1={require('../../../resources/hinh_nen_log_In.png')}
+            />
+            <AppInput
+                placeholder={'Nhập email hoặc số điện thoại'}
+                styles={{
+                    containerFocus: getContainerInputFocusStyle(),
+                    containerUnFocus: getContainerInputUnFocusStyle(),
+                    icon: getIconEyeStyle(),
+                    inputText:getInputTextStyle()
+                }}
+            />
+
+            <AppInput
+                placeholder={'Mật khẩu'}
+                styles={{
+                    containerFocus: getContainerInputFocusStyle(),
+                    containerUnFocus: getContainerInputUnFocusStyle(),
+                    icon: getIconEyeStyle(),
+                    inputText:getInputTextStyle()
+                }}
+                isPassword={true}
             />
         </View>
     )
